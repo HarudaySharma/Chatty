be24eebe8b9bb5fb93309e5f4592d6180d6e0bd5
HarudaySharma <harudayvasist@gmail.com>
2024-06-04 18:55:45 +0530

    feat: implement basic user page design

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,16 +1,13 @@
-'use client'
-
-import { signOut } from 'next-auth/react'
 import React from 'react'
+import EmptyState from '../components/EmptyState'
 
 const Users = () => {
     return (
-        <button
-            onClick={() => signOut()}
-            className='p-4 bg-sky-50'
+        <div
+            className={`hidden lg:block lg:pl-80 h-full`}
         >
-            LogOut
-        </button>
+            <EmptyState />
+        </div>
     )
 }
 
