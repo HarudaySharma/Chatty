6130117676cc3fdb2a1e277fc6d9747a9c5d8241
HarudaySharma <harudayvasist@gmail.com>
2024-06-05 16:34:22 +0530

    feat: add next-superjson-plugin

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        swcPlugins: [
+            ['next-superjson-plugin', {}]
+        ],
+    },
+    images: {
+        domains: [
+            'res.cloudinary.com',
+            'avatars.githubusercontent.com',
+            'lh3.googleusercontent.com',
+        ]
+    }
+};
+
+export function middleware(request) {
+    console.log('Middleware called for:', request.url);
+    return NextResponse.next();
+}
+
 
 export default nextConfig;
